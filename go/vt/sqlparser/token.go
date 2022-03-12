975a29cc13a24175ad0c728e26a1094b097c2a98
General Kroll <generalkroll0@gmail.com>
2022-03-12 11:58:58 +1100

    added registry
    Signed-off-by: General Kroll <generalkroll0@gmail.com>

--- go/vt/sqlparser/token.go
+++ go/vt/sqlparser/token.go
@@ -245,6 +245,7 @@ var keywords = map[string]int{
 	"less":                LESS,
 	"level":               LEVEL,
 	"like":                LIKE,
+	"list":                LIST,
 	"limit":               LIMIT,
 	"linear":              UNUSED,
 	"lines":               UNUSED,
